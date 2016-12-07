18d57e54cd2ba80a3bec9a8f9ad6a05607c6b03c
DrWilliamsSteven <sw17@sanger.ac.uk>
2016-12-07 15:50:02 +0000

    Update layout and js functions

---
+++ codepenclone.js
@@ -0,0 +1,22 @@
+$(document).ready(function() {
+			
+			function updateOutput() {
+				$('iframe').contents().find('html').html(
+					'<html><head><style type="text/css">' + $('#cssinputbox').val() + '</style></head><body>' + 	$('#htmlinputbox').val()  + '</body></html>' );
+					
+				document.getElementById('outputbox').contentWindow.eval($('#jsinputbox').val() );	
+			}
+
+			$('.panel').height($(window).height() -  $('#toolbar').height() - $('.menubar').height() - 65); 	//
+			$('textarea').height($('.panel').height() -  $('.menubar').height() -10); 	
+
+			$('.panel').width($(window).width() / 4 - 10 );
+
+			updateOutput()
+
+			$('textarea').on('change keyup paste', function() {
+				updateOutput()
+			});			
+
+
+});
