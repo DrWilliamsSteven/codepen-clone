5c3dcf1e7a692bf58599c599605a0e1734b04c05
Steven Williams <dr.williams.steven@gmail.com>
2016-12-07 16:06:22 +0000

    Update codepenclone.js

--- codepenclone.js
+++ codepenclone.js
@@ -1,14 +1,13 @@
 $(document).ready(function() {
-<<<<<<< HEAD
 			
 			function updateOutput() {
 				$('iframe').contents().find('html').html(
-					'<html><head><style type="text/css">' + $('#cssinputbox').val() + '</style></head><body>' + 	$('#htmlinputbox').val()  + '</body></html>' );
+					'<html><head><style type="text/css">' + $('#cssinputbox').val() + '</style></head><body>' + $('#htmlinputbox').val()  + '</body></html>' );
 					
 				document.getElementById('outputbox').contentWindow.eval($('#jsinputbox').val() );	
 			}
 
-			$('.panel').height($(window).height() -  $('#toolbar').height() - $('.menubar').height() - 65); 	//
+			$('.panel').height($(window).height() -  $('#toolbar').height() - $('.menubar').height() - 65); 	
 			$('textarea').height($('.panel').height() -  $('.menubar').height() -10); 	
 
 			$('.panel').width($(window).width() / 4 - 10 );
@@ -18,21 +17,5 @@ $(document).ready(function() {
 			$('textarea').on('change keyup paste', function() {
 				updateOutput()
 			});			
-
-=======
-
-
-
-$('.panel').height($(window).height() -  $('#toolbar').height() - $('.menubar').height() - 65); 	//
-$('textarea').height($('.panel').height() -  $('.menubar').height() -10); 	
-
-$('.panel').width($(window).width() / 4 - 5 );
-
-$('iframe').contents().find('html').html($('#htmlinputbox').val()  );
-
-$('textarea').on('change keyup paste', function() {
-	$('iframe').contents().find('html').html($('#htmlinputbox').val()  );
-});
->>>>>>> origin/master
 
 });
